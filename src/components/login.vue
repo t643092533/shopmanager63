6fa48199424270cd484f926c3bb79ad72f8a03d3
lzz <447089649@qq.com>
2019-02-13 11:34:21 +0800

    登录-发送请求1

--- src/components/login.vue
+++ src/components/login.vue
@@ -8,7 +8,7 @@
       <el-form-item label="密码">
         <el-input v-model="formdata.password"></el-input>
       </el-form-item>
-      <el-button class="login-btn" type="primary">登录</el-button>
+      <el-button @click.prevent="handlelogin()" class="login-btn" type="primary">登录</el-button>
     </el-form>
   </div>
 </template>
@@ -22,9 +22,56 @@ export default {
         password: ""
       }
     };
+  },
+  methods: {
+    // 登录请求
+
+    handlelogin() {
+      this.$http
+        .post(`login`, this.formdata)
+        .then(res => {
+          console.log(res);
+          const {
+            data: {
+              data,
+              meta: { msg, status }
+            }
+          } = res;
+          if (status === 200) {
+            console.log("login---success----");
+          } else {
+            console.log("error----");
+          }
+
+          // eg
+          // const {per} = {per:"abc"}
+          // console.log(per)
+          // const {name:newname} = {name:'abc'}
+          // console.log(newname);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
   }
 };
 </script>
 
 <style>
+.login-wrap {
+  height: 100%;
+  background-color: #324152;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.login-form {
+  background-color: #ffffff;
+  width: 400px;
+  border-radius: 5px;
+  padding: 30px;
+}
+.login-btn {
+  width: 100%;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import '@/assets/css/base.css'
 import App from './App'
 import router from './router'
+import axios from 'axios'
+axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
+Vue.prototype.$http = axios
 
 // ElementUI其实Vue插件,和VueRouter用法一样  Vue.use(名)
 // Vue插件的使用
